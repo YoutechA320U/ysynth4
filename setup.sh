593b4e0b4296dc398dedc56f509233aff741eb40
YoutechA320U <42425276+YoutechA320U@users.noreply.github.com>
2019-08-23 12:01:25 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+sudo apt-get -y update 
+sudo apt-get -y upgrade 
+sudo apt-get -y autoremobe
+sudo apt-get install -y libasound2-dev git build-essential python3-dev libpython3.7-dev libjack-jackd2-dev cython3 python3-setuptools i2c-tools python3-smbus python3-rpi.gpio python3-pip timidity fluid-soundfont-gm python3-rpi.gpio python3-spidev python3-pip python3-numpy build-essential libjpeg-dev debhelper fonts-takao-gothic
+sudo raspi-config nonint do_i2c 0
+sudo raspi-config nonint do_spi 0 
+sudo raspi-config nonint do_uart 0
+
+sudo sed -i -e '$ a dtparam=i2s=on' /boot/config.txt
+sudo sed -i -e '/#dtparam=i2s=on/d' /boot/config.txt
+sudo sed -i -e '$ a dtoverlay=pi3-miniuart-bt' /boot/config.txt
+sudo sed -i -e 'dtoverlay=midi-uart0' /boot/config.txt
+sudo sed -i -e 's/console=serial0,115200//' /boot/cmdline.txt
+
 cd /home/pi/ysynth4/
 rm timidity_2.14.0.orig.tar.bz2*
 wget http://deb.debian.org/debian/pool/main/t/timidity/timidity_2.14.0.orig.tar.bz2
@@ -21,4 +35,8 @@ git clone https://github.com/rbrito/usbmount.git
 cd /home/pi/usbmount
 sudo dpkg-buildpackage -us -uc -b
 cd ..
-sudo apt install  ./usbmount_0.0.24_all.deb
+sudo apt install -y ./usbmount_0.0.24_all.deb
+git clone https://github.com/YoutechA320U/ttymidi
+cd /home/pi/ttymidi
+gcc ttymidi.c -o ttymidi -lasound -pthread
+mv ttymidi /home/pi/ysynth4
